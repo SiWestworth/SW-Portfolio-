2b205548d1db315922efa532904036f34f44efae
CycloneBlue <sclw@me.com>
2023-11-26 15:44:18 +0000

    Very bassic framework for site ready to build on. git commit

---
+++ Main/Java.js
@@ -0,0 +1,9 @@
+// Add your JavaScript code here
+document.addEventListener("DOMContentLoaded", function () {
+  // Example: Display a pop-up when a button is clicked
+  const myButton = document.getElementById("myButton");
+
+  myButton.addEventListener("click", function () {
+    alert("Hello! This is a pop-up message.");
+  });
+});
