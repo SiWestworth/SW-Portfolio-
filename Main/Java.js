800aac80bd0f6e69e34d10e1a7bd3f84ab57245a
Simon Westworth <sclw@me.com>
2023-12-07 20:45:04 +0000

    basic layout and styling

--- Main/Java.js
+++
@@ -1,9 +0,0 @@
-// Add your JavaScript code here
-document.addEventListener("DOMContentLoaded", function () {
-  // Example: Display a pop-up when a button is clicked
-  const myButton = document.getElementById("myButton");
-
-  myButton.addEventListener("click", function () {
-    alert("Hello! This is a pop-up message.");
-  });
-});
